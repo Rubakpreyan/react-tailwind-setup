4b4c9ecf2517e5f1241d092ebe1b6f91c114bb4d
G. Rubak preyan <75318125+Rubakpreyan@users.noreply.github.com>
2021-07-15 15:55:46 +0530

    Updated total setup
    Added purged files

---
+++ src/Components/Tesing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tesing = () => {
+  return <div>This is an empty Js component</div>;
+};
+
+export default Tesing;
---
+++ src/Components/testing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const testing = () => {
+  return <div>This is an empty JSX component</div>;
+};
+
+export default testing;
